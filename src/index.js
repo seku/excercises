c7515afe0c70e96e6cebe2b61aa6b3fa68caa8e9
sekupyna <sekupyna@gmail.com>
2018-01-13 22:46:27 +0100

    introuduce router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
-import Dashboard from './components/dashboard'
+import App from './components/app'
 
 ReactDOM.render(
-  <Dashboard />,
+  <App />,
   document.getElementById('content')
 );
